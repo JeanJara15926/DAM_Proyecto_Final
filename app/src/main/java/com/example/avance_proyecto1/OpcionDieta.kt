3806aa85b07589aded25583825bd80a8ddd5348e
123JHONY123 <2015100243@untels.edu.pe>
2022-07-24 18:47:05 -0500

    hola

--- app/src/main/java/com/example/avance_proyecto1/OpcionDieta.kt
+++ app/src/main/java/com/example/avance_proyecto1/OpcionDieta.kt
@@ -46,25 +46,30 @@ class OpcionDieta : AppCompatActivity() {
             binding.nombre.text="Dieta EQUILIBRADA"
             val id = resources.getIdentifier("equilibrada", "drawable", packageName)
             binding.imageView4.setImageResource(id)
-            binding.textd.text="Dieta EQUILIBRADAsadasdasdas"
+            binding.textd.text= "Una alimentación equilibrada supone un aporte de nutrientes adecuado a las necesidades individuales de cada persona para el mantenimiento de la salud y debe cubrir las demandas energéticas del organismo.\n" +
+                    "Las dietas variadas permiten asegurar la ingestión proporcionada de todos los nutrientes, debiendo ajustar individualmente las calorías necesarias en función de la actividad desarrollada.\n" +
+                    "Otro de los aspectos que debe tenerse en cuenta son las posibles variaciones individuales por componentes genéticos, ambientales."
 
         }else if(nombre=="MEDITERRANEA"){
             binding.nombre.text="Dieta MEDITERRANEA"
             val id = resources.getIdentifier("medit", "drawable", packageName)
             binding.imageView4.setImageResource(id)
-            binding.textd.text="Dieta EQUILIBdsadasdasdRADAsadasdasdas"
+            binding.textd.text="Se trata de una alimentación variada, sin exceder las necesidades diarias de cada individuo, en función de su edad, sexo, talla y actividad física.\n" +
+                                "Se recomienda el consumo de carbohidratos en cantidad de hasta 300-400 g./día, sobre todo en forma de legumbres, verduras y frutas.\n" +
+                                "Es preciso evitar la grasa animal, a excepción de la que proviene de pescados marinos y pequeños aportes de lácteos, con consumo preferente de aceite de oliva en cantidad diaria que oscila de 70 a 80 gramos."
 
         }else if(nombre=="GRASAS"){
             binding.nombre.text="Dieta ALTO EN GRASAS"
             val id = resources.getIdentifier("altograsas", "drawable", packageName)
             binding.imageView4.setImageResource(id)
-            binding.textd.text="Dieta EQUILIBRasdasdasADAsadasdasdas"
+            binding.textd.text="Las grasas aportan energía y ayudan a absorber determinados nutrientes. Cada gramo de grasa nos proporciona 9 kcal. mientras que un gramo de carbohidratos sólo nos proporciona 4 kcal.\n" +
+                                "Se recomienda que las grasas de la dieta aporten entre un 20 y un 30% de las necesidades energéticas diarias."
 
         }else if(nombre=="PROTEINAS"){
             binding.nombre.text="Dieta ALTO EN PROTEINAS"
             val id = resources.getIdentifier("proteina", "drawable", packageName)
             binding.imageView4.setImageResource(id)
-            binding.textd.text="Dieta EQUILIBRADAsadddasdasdas"
+            binding.textd.text="Las proteínas son los nutrientes que desempeñan un mayor número de funciones en las células de todos lo seres vivos. Forman parte de la estructura de los tejidos y por otro lado tienen función metabólica y reguladora."
 
         }
 
